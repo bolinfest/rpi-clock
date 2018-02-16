232f4eccc8c9230ddcc6d7591c2311e6ccbb606c
Michael Bolin <bolinfest@gmail.com>
2018-02-16 11:30:06 -0800

    Created a simple mobile UI to toggle clock modes.

--- src/expo/App.js
+++ src/expo/App.js
@@ -1,14 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>For now, your only options are to toggle between clock and counter</Text>
+        <Button
+          title="Clock"
+          onPress={useClockMode}
+        />
+        <Button
+          title="Counter"
+          onPress={useCounter}
+        />
       </View>
     );
   }
+}
+
+async function useClockMode() {
+  await fetch('http://raspberrypi.local:8081/clock', {
+    method: 'POST',
+    headers: {
+      'X-XSRF': '1',
+    },
+  });
+}
+
+async function useCounter() {
+  await fetch('http://raspberrypi.local:8081/count_up', {
+    method: 'POST',
+    headers: {
+      'X-XSRF': '1',
+    },
+  });
 }
 
 const styles = StyleSheet.create({
