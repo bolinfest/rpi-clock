f7e87ef0f2a2e65d4dcdc13b8d75fd5cc89232e9
Michael Bolin <bolinfest@gmail.com>
2018-02-16 17:20:44 -0800

    Make 24hr time a setting and default to 12hr.

--- src/service/ClockThread.py
+++ src/service/ClockThread.py
@@ -4,6 +4,10 @@ import datetime
 
 
 class ClockThread(DisplayThread):
+    def __init__(self, controller):
+        super(ClockThread, self).__init__(controller)
+        self._is_24_hour_time = False
+
     def run(self):
         colon = False
         while not self.interrupted:
@@ -11,8 +15,18 @@ class ClockThread(DisplayThread):
             hour = now.hour
             minute = now.minute
 
+            if self._is_24_hour_time:
+                # Use leading zeroes for the hour in 24h time.
+                fmt = '{:02d}{:02d}'
+            else:
+                fmt = '{:2d}{:02d}'
+                if hour == 0:
+                    hour = 12
+                elif hour > 12:
+                    hour -= 12
+
             # TODO: Support 12 vs 24-hour time.
-            d0, d1, d2, d3 = '{:2d}{:02d}'.format(hour, minute)
+            d0, d1, d2, d3 = fmt.format(hour, minute)
             colon = not colon
 
             self.update(d0, d1, d2, d3, colon)
